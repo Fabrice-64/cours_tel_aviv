20d7a3c62daa3062b5bb880dc3d89915d80e3f50
Fabrice-64 <fab.jaouen@gmail.com>
2022-01-06 16:05:53 +0100

    character frequency analysis

---
+++ 09_frequency_analysis.py
@@ -0,0 +1,55 @@
+from collections import Counter
+
+ENG_1 = """
+Do in laughter securing smallest sensible no mr hastened. 
+As perhaps proceed in in brandon of limited unknown greatly. 
+Distrusts fulfilled happiness unwilling as explained of difficult. 
+No landlord of peculiar ladyship attended if contempt ecstatic. 
+Loud wish made on is am as hard. Court so avoid in plate hence. 
+Of received mr breeding concerns peculiar securing landlord. 
+Spot to many it four bred soon well to. Or am promotion in no departure abilities. 
+Whatever landlord yourself at by pleasure of children be.
+"""
+
+ENG_2 = """
+Increasing impression interested expression he my at. 
+Respect invited request charmed me warrant to. 
+Expect no pretty as do though so genius afraid cousin. 
+Girl when of ye snug poor draw. Mistake totally of in chiefly. 
+Justice visitor him entered for. 
+Continue delicate as unlocked entirely mr relation diverted in. 
+Known not end fully being style house. 
+An whom down kept lain name so at easy.
+"""
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+most_freq_eng = "etaion"
+
+
+def is_ascii(s):
+    return s.isascii()
+
+
+def is_english(dict_chars):
+    counter = 0
+    desc_freq_chars = list(sorted(dict_chars.items(), key=lambda x: x[1], reverse=True))
+    for item in desc_freq_chars:
+        if item[0] not in alphabet:
+            desc_freq_chars.remove(item)
+    for item in desc_freq_chars[:3]:
+        if item[0] in most_freq_eng:
+            counter += 1
+    if counter == 3:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    if is_ascii(ENG_2):
+        ENG_2 = ENG_2.lower()
+        count_char = Counter(ENG_1)
+        value = is_english(count_char)
+        print(value)
+    else:
+        print("Your string includes non ASCII Characters. Please correct it before proceeding.")
--- README.md
+++ README.md
@@ -14,4 +14,7 @@ Those packages are listed in requirements.txt
 - 05: Encrypt a string using XOR based cipher and a one time pad (keystream bit used only once)
 - 06: fake a RC4 encryption. Model is taken from https://github.com/manojpandey/rc4
 - 07: encrypt a message using RC4 with the library pycryptodome. Source in Python2: https://github.com/nemozqqz/pycrypto-sample/blob/master/RC4.py
-
+- 08: encrypt a message using AES and CBC, based on pycryptodome
+- 08_v2: a simplified version of the previous exer, dedicated to TAU exercise. Interesting as a breakdown of the previous exercise.
+- 09: counts the character frequency in a large string iot to detect whether it's in ENG language.
+- 10: crack through brute force an AES_CBC encrypted phone number
